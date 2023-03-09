a526dd1f553e5f7d6da42df78c1375bcbc43319a
L3XxXa <alexkkk@inbox.ru>
2023-03-09 17:19:48 +0700

    List of releases page init

--- release_system/src/components/index.js
+++ release_system/src/components/index.js
@@ -1,9 +1,11 @@
 import navigationMenuButton from "@/components/UI/buttons/NavigationMenuButton";
 import navigationMenu from "@/components/navigationMenu/NavigationMenu";
 import releaseButton from "@/components/UI/buttons/ReleaseButton";
+import releaseCard from "@/components/ReleaseCard";
 
 export default [
     navigationMenuButton,
     navigationMenu,
-    releaseButton
+    releaseButton,
+    releaseCard
 ]
--- release_system/src/pages/ListOfReleasesPage.vue
+++ release_system/src/pages/ListOfReleasesPage.vue
@@ -1,26 +1,50 @@
 <template>
-<div>
-  <h1>
-    Здесь будет список релизов
-  </h1>
-  <span>Но пока тут котик</span>
-</div>
-  <img src="@/assets/img/kitten.jpg">
+  <div>
+    <div class="nav__menu">
+      <navigation-menu></navigation-menu>
+    </div>
+    <div class="content">
+      <h1 class="heading">
+        Список релизов
+      </h1>
+      <release-card>
 
+      </release-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import ReleaseCard from "@/components/ReleaseCard";
 export default {
-  name: "ListOfReleasesPage"
+  name: "ListOfReleasesPage",
+  components: {ReleaseCard}
 }
 </script>
 
 <style scoped>
-  h1{
-    font-family: Montserrat;
-  }
-  span{
-    font-family: Montserrat;
-    font-size: 20px;
-  }
+.nav__menu {
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  float: left;
+  width: 17%;
+  margin-left: 4px;
+  margin-top: 4px;
+}
+
+.content{
+  float: right;
+  width: 83%;
+  margin-top: 30px;
+}
+
+.heading{
+  font-family: Montserrat;
+  font-weight: normal;
+  margin-left: 30%;
+}
 </style>
